7cccb63f2e56ea041e9e8acbc59cf3d65511c531
Ryan Stewart <Ryanps914@yahoo.com>
2022-03-12 14:47:14 -0500

    Added Users table

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { User } from '../models/user.model';
+import { UserService } from '../services/user.service';
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+  styleUrls: ['./user-list.component.css'],
 })
 export class UserListComponent implements OnInit {
+  users: User[] = [];
 
-  constructor() { }
+  constructor(private userService: UserService) {}
 
   ngOnInit(): void {
+    this.userService.getAll().subscribe(
+      (data) => {
+        console.log(data)
+        this.users = data;
+      },
+      (error) => {console.log(error) }
+    );
   }
-
 }

d8670ca93e10a2227c8d4d704437150dba945bc7
Ryan Stewart <Ryanps914@yahoo.com>
2022-03-12 16:21:16 -0500

    modified user-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
+
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { UserListComponent } from './features/user-list/user-list.component';
+import { AboutComponent } from './features/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    UserListComponent,
     AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
